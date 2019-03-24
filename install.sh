a1417061cc5c1874e0b2a96b34b06ceb02629678
ryodocx <email@ryodocx.net>
2019-03-24 13:28:03 +0900

    update

--- install.sh
+++ install.sh
@@ -1,14 +1,53 @@
-#!/bin/bash
-set -ex
-
+#!/usr/bin/env bash
+set -e
+cd $(dirname $0)
 ################################################################################
 # asdf
 (
     if [ ! -d ~/.asdf ]; then
         git clone https://github.com/asdf-vm/asdf.git ~/.asdf
     fi
-    cd ~/.asdf
-    git fetch --all
-    git checkout "$(git describe --abbrev=0 --tags)"
+    . ~/.asdf/asdf.sh
+    . ~/.asdf/completions/asdf.bash
+    asdf update
+
+    case "$(uname -s)" in
+    "Darwin")
+        brew install \
+            coreutils \
+            automake \
+            autoconf \
+            openssl \
+            libyaml \
+            readline \
+            libxslt \
+            libtool \
+            gpg \
+            unixodbc ||
+            :
+        # sudo installer -pkg /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14.pkg -target /
+        ;;
+    "Linux")
+        # ubuntu
+        #   sudo apt install \
+        #   automake autoconf libreadline-dev \
+        #   libncurses-dev libssl-dev libyaml-dev \
+        #   libxslt-dev libffi-dev libtool unixodbc-dev
+        ;;
+    esac
+
+    function asdf-plugin-add() {
+        toolName=$1
+        asdf plugin-add ${toolName} || :
+    }
+
+    asdf-plugin-add terraform
+    asdf-plugin-add kubectl
+    asdf-plugin-add helm
+    asdf-plugin-add python && asdf install python 2.7.16
+    asdf-plugin-add golang
+    asdf-plugin-add nodejs && bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+    cd ~
+    asdf install
 )
 ################################################################################
