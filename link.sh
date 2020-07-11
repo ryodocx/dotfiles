c5d7d3245f7d493c77283916584796b387616b97
ryodocx <email@ryodocx.net>
2020-07-11 16:29:38 +0900

    update ssh config

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@
 cd $(dirname $0)
 ln -sfnv $(pwd)/bin/ ~/.bin
 ln -sfnv $(pwd)/rcfiles/ ~/.rcfiles
+ln -sfnv $(pwd)/ssh_conf.d/ ~/.ssh/conf.d
 
 for filepath in $(find files -type f); do
     filepath=${filepath#files/}
